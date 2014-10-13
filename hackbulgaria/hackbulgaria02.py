3f30283f784f94c1bc3708932b4614a2d47d0f61
kalpak44 <avgustares@gmail.com>
2014-10-13 18:50:15 +0300

    add simpl fraction

--- hackbulgaria/hackbulgaria02.py
+++ hackbulgaria/hackbulgaria02.py
@@ -2,14 +2,14 @@
 array = ["apple", "banana", "apple", "pie"]
 
 def count_words(arr):
-	words = {}
-	for word in arr:
-		print (word)
-		if words.get(word) == None:
-			words[word] = 1
-		else:
-			words[word] = words[word] + 1
-	return words
+    words = {}
+    for word in arr:
+        print (word)
+        if words.get(word) == None:
+            words[word] = 1
+        else:
+            words[word] = words[word] + 1
+    return words
 
 
 #print (count_words(array))
@@ -18,7 +18,7 @@ def count_words(arr):
 
 #Unique words
 def unique_words_count(arr):
-	return len(set(arr))
+    return len(set(arr))
 
 #print (unique_words_count(array))
 
@@ -30,17 +30,17 @@ def unique_words_count(arr):
 
 #Spam and Eggs
 def prepare_meal(number):
-	num = number
-	result = ""
-	if not (number % 3 == 0):
-		return result
-	else:
-		while (number % 3 == 0):
-			number = number / 3
-			result = result + "spam "
-	if num % 5 == 0:
-		result = result + "and eggs"
-	return result
+    num = number
+    result = ""
+    if not (number % 3 == 0):
+        return result
+    else:
+        while (number % 3 == 0):
+            number = number / 3
+            result = result + "spam "
+    if num % 5 == 0:
+        result = result + "and eggs"
+    return result
 
 #print (prepare_meal(45))
 
@@ -48,44 +48,72 @@ def prepare_meal(number):
 #Reduce file path
 str = '/srv/www/./htdocs//wtf/..'
 def reduce_file_path(path):
-	path = str.split('/')
-	i = 0
-	result = ""
-	for n in path:
-		if path[i] == '':
-			path.remove(path[i])
-		elif path[0] == '..' or path[1] == '.':
-			return '/'
-		elif path[i] == '..' and i != 0:
-			path.remove(path[i])
-			path.remove(path[i-1])
-		elif path[i] == '.':
-			path.remove(path[i])
-		i = i + 1
+    path = str.split('/')
+    i = 0
+    result = ""
+    for n in path:
+        if path[i] == '':
+            path.remove(path[i])
+        elif path[0] == '..' or path[1] == '.':
+            return '/'
+        elif path[i] == '..' and i != 0:
+            path.remove(path[i])
+            path.remove(path[i-1])
+        elif path[i] == '.':
+            path.remove(path[i])
+        i = i + 1
 
-	str_return = ''
-	for directory in path:
-		str_return = str_return + '/' + directory
+    str_return = ''
+    for directory in path:
+        str_return = str_return + '/' + directory
 
 
-	return str_return
+    return str_return
 
 #print (reduce_file_path(str))
 
 #Word from a^nb^n
+
+
 def is_an_bn(word):
-	myword = list(word)
-	if len(myword) % 2 != 0:
-		return False
-	else:
-		middle = len(myword) / 2
-		i = 0
-		while i < middle:
-			if myword[i] != 'a' or myword[len(myword)-i-1] !='b':
-				return False
+    myword = list(word)
+    if len(myword) % 2 != 0:
+        return False
+    else:
+        middle = len(myword) / 2
+        i = 0
+        while i < middle:
+            if myword[i] != 'a' or myword[len(myword)-i-1] != 'b':
+                return False
 
-			i = i + 1
-		return True
+            i = i + 1
+        return True
 
 
-print(is_an_bn("aabbaabb"))
+#print(is_an_bn("aabbaabb"))
+
+
+#Simplify fractions
+
+class Fraction():
+    def __init__(self, numerator, denumerator):
+        self.numerator = numerator
+        self.denumerator = denumerator
+    def __str__(self):
+        return "{} / {}".format(self.numerator, self.denumerator)
+
+
+def simplify_fraction(fraction):
+    nominator = fraction.numerator
+    denominator = fraction.denumerator
+    r = 0
+    mcd = 0
+    while denominator != 0:
+        r = nominator % denominator;
+        nominator = denominator;
+        denominator = r;
+    mcd = nominator
+    return Fraction(fraction.numerator / mcd,fraction.denumerator / mcd)
+
+f = Fraction(3,9)
+print (simplify_fraction(f))
